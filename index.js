d3712a49127b5ca2f48ac3ee10d0cc22ab416ba5
System Administrator <root@iOSDeveloperui-iMac.local>
2016-07-18 22:24:43 +0900

    ddd

--- index.js
+++ index.js
@@ -1,53 +1,131 @@
 'use strict'
 
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
 
-var Config = require('./config')
-var FB = require('./connectors/facebook')
-var Bot = require('./bot')
+app.set('port', (process.env.PORT || 5000))
 
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
-// LETS MAKE A SERVER!
-var app = express()
-app.set('port', (process.env.PORT) || 5000)
-// SPIN UP SERVER
-app.listen(app.get('port'), function () {
-  console.log('Running on port', app.get('port'))
-})
-// PARSE THE BODY
+// Process application/json
 app.use(bodyParser.json())
 
-
-// index page
+// Index route
 app.get('/', function (req, res) {
-  res.send('안녕, 여기는 챗봇윗')
+    res.send('안녕하세요.  인공지능 챗봇고 입니다!!!')
 })
 
-// for facebook to verify
-app.get('/webhooks', function (req, res) {
-  if (req.query['hub.verify_token'] === Config.FB_VERIFY_TOKEN) {
-    res.send(req.query['hub.challenge'])
-  }
-  res.send('Error, wrong token')
-})
-
-// to send messages to facebook
-app.post('/webhooks', function (req, res) {
-  var entry = FB.getMessageEntry(req.body)
-  // IS THE ENTRY A VALID MESSAGE?
-  if (entry && entry.message) {
-    if (entry.message.attachments) {
-      // NOT SMART ENOUGH FOR ATTACHMENTS YET
-      FB.newMessage(entry.sender.id, "That's interesting!")
-    } else {
-      // SEND TO BOT FOR PROCESSING
-      Bot.read(entry.sender.id, entry.message.text, function (sender, reply) {
-        FB.newMessage(sender, reply)
-      })
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
     }
-  }
-
-  res.sendStatus(200)
+    res.send('Error, wrong token')
 })
+
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            if (text === 'Generic') {
+                sendGenericMessage(sender)
+                continue
+            }
+            sendTextMessage(sender, "챗봇고, echo: " + text.substring(0, 200))
+        }
+
+       if (event.postback) {
+         let text = JSON.stringify(event.postback)
+         sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+         continue
+       }
+
+
+
+    }
+    res.sendStatus(200)
+})
+
+
+
+const token = process.env.FB_PAGE_ACCESS_TOKEN
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
+
+function sendGenericMessage(sender) {
+    let messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
+    }
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+    })
+}
