fdec79297e185c0ebe72bafc4a870d8ca9fa580d
System Administrator <root@iOSDeveloperui-iMac.local>
2016-07-19 07:08:34 +0900

    aaaa

--- index.js
+++ index.js
@@ -50,9 +50,8 @@ app.post('/webhook/', function (req, res) {
 })
 
 
-// recommended to inject access tokens as environmental variables, e.g.
-// const token = process.env.PAGE_ACCESS_TOKEN
-const token = "<PAGE_ACCESS_TOKEN>"
+// recommended to inject access tokens as environmental variables, e.g. 
+const token = process.env.PAGE_ACCESS_TOKEN
 
 function sendTextMessage(sender, text) {
 	let messageData = { text:text }
